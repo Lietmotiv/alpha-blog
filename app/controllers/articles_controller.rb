cfa84861edf688e130939a8b95e7782e522bffa0
Doug Clayton <doug@xnegenx.com>
2016-04-22 23:23:29 +0000

    Extract redundancy and prod push

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:edit,:update,:show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -9,7 +12,8 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+
+
   end
   
   def create
@@ -27,13 +31,11 @@ class ArticlesController < ApplicationController
   
   def show
     
-    @article = Article.find(params[:id])
-    
+
   end
   
   def update
     
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] ="Article was updated"
       redirect_to article_path(@article)
@@ -45,7 +47,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     flash[:notice] = "Article was deleted"
@@ -53,6 +54,9 @@ class ArticlesController < ApplicationController
   end
   
   private 
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
